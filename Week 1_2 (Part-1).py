a278938986acf67616a706525c2f44356b2580cc
blokesh24@tbc.edu.np <blokesh24@tbc.edu.np>
2025-04-14 22:28:48 +0545

    added the answers in the part 1 file

---
+++ Week 1_2 (Part-1).py
@@ -0,0 +1,63 @@
+# week 1-2, p1:
+#**************
+
+"""Here The Questions are given in the multi-line comments using quotations and answers are given in single line comments using #"""
+
+
+"""1)WAP to calculate the perimeter of rectangle whose length is
+    9 and breadth is 6.
+"""
+# p = 2 * (9 + 6)
+# print("Perimeter:", p)
+
+"""2) WAP to print:'Python is great, it’s wild!' """
+
+
+# print("Python is great, its wild!")
+
+
+
+"""3)WAP to print 2 to the 10th power """
+
+# power = 2 ** 10
+# print("2 to the 10th power:", power)
+
+
+"""4) WAP to print the difference of 7 factorial and 5 factorial. """
+
+# import math
+# factorial_difference = math.factorial(7) - math.factorial(5)
+# print("7! - 5!:", factorial_difference)
+
+
+"""5) WAP to print your forename multiplied by 5 """
+
+# forename = "Bhatta"
+# print("Forename * 5:", forename * 5)
+
+
+"""6) WAP print your name left justified 15 spaces """
+
+# print("Lokesh".ljust(15))
+
+"""7) WAP to print the value of PI to 5 decimal places """
+
+# import math
+# pi_value = round(math.pi, 5)
+# print("PI to 5 decimal places:", pi_value)
+
+"""8)  WAP to print the value of 200 modulus 12  """
+
+# modulus_result = 200 % 12
+# print("200 % 12:", modulus_result)
+
+"""9) WAP to print 7.2 as an integer value """
+
+# a = int(7.2)
+# print("Integer value of 7.2:", a)
+
+
+"""10) WAP to print the Unicode encoding for your name """
+
+# unicode_values = [ord(char) for char in "Lokesh"]
+# print("Unicode encoding for your name:", unicode_values)
